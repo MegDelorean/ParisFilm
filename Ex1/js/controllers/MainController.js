4597e98a2935575c95274c03cfe59163947e25d6
Charlotte <charlotte.caseau@gmail.com>
2016-01-18 20:47:48 +0100

    essai map

--- Ex1/js/controllers/MainController.js
+++ Ex1/js/controllers/MainController.js
@@ -1,44 +1,23 @@
 app.controller('MainController', ['$scope', function($scope) { 
-  $scope.title = 'This Month\'s Bestsellers'; 
-  $scope.promo = 'The most popular books this month.';
-  $scope.products = [
+  $scope.title = 'Meilleurs films'; 
+  $scope.promo = 'Decouvrez les  films tournés à Paris'
+  $scope.films = [
   	{ 
-    	name: 'The Book of Trees', 
-    	price: 19, 
-    	pubdate: new Date('2014', '03', '08'), 
-    	cover: 'img/the-book-of-trees.jpg',
-    	likes: 0,
-    	dislikes: 0
-  	}, 
-  	{ 
-    	name: 'Program or be Programmed', 
-    	price: 8, 
-    	pubdate: new Date('2013', '08', '01'), 
-    	cover: 'img/program-or-be-programmed.jpg',
-    	likes: 0,
-    	dislikes: 0
-  	}, 
-  	{ 
-    	name: 'Harry Potter & The Prisoner of Azkaban', 
-    	price: 11.99, 
-    	pubdate: new Date('1999', '07', '08'), 
-    	cover: 'http://upload.wikimedia.org/wikipedia/en/b/b4/Harry_Potter_and_the_Prisoner_of_Azkaban_(US_cover).jpg',
-    	likes: 0,
-    	dislikes: 0 
-  	}, 
-  	{ 
-    	name: 'Ready Player One', 
-    	price: 7.99, 
-    	pubdate: new Date('2011', '08', '16'), 
-    	cover: 'http://upload.wikimedia.org/wikipedia/en/a/a4/Ready_Player_One_cover.jpg',
+    	titre: 'Le fabuleux destin d Amelie Poulain', 
+    	realisateur: 'Jean-Pierre JEUNET', 
+    	date: '2000', 
+    	cover: 'http://ecx.images-amazon.com/images/I/71sBZgwRBYL._SL1300_.jpg',
+		synopsis :'Le synopsis du film...',
+		acteurs :'Audrey TAUTOU',
+		lieux :'Rue Lepic (18e),Rue Mouffetard (5e),Rue des Trois Freres (18e)',
     	likes: 0,
     	dislikes: 0 
   	}
   ];
   $scope.plusOne = function(index) { 
-  	$scope.products[index].likes += 1; 
+  	$scope.films[index].likes += 1; 
 	};
 	$scope.minusOne = function(index) { 
-  	$scope.products[index].dislikes += 1; 
+  	$scope.films[index].dislikes += 1; 
 	};
 }]);
