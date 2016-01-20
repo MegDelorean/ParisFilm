e26e62e83d2f2f9793fb891577182d1566f6d49d
Alice <alice.maixent@gmail.com>
2016-01-20 15:16:17 +0100

    Réunion de géolocalisation et données

--- mapfilms/js/controllers/MapCtrl.js
+++ mapfilms/js/controllers/MapCtrl.js
@@ -1,47 +1,80 @@
-
 //Angular App Module and Controller
 
-app.controller('MapCtrl',['$scope', 'filmsDataService', function($scope, filmsDataService){
-    filmsDataService.getFilms().then(function(data){
-        for (i = 0; i < data.data.length; i++){
-            if(data.data[i].lat !== 0) { createMarker(data.data[i]) };
+app.controller('MapCtrl',['$scope', 'filmsDataService', '$window', function($scope, filmsDataService, $window){
+$window.navigator.geolocation.getCurrentPosition(function(position) {
+
+        filmsDataService.getFilms().then(function(data){
+            for (i = 0; i < data.data.length; i++){
+                if(data.data[i].lat !== 0) { createMarker(data.data[i]) };
+            }
+            $scope.movies = data.data;
+        })
+
+
+        /*  var lat = position.coords.latitude;
+            var lng = position.coords.longitude; */
+        // On code lat et lng en dur pour simuler que nous nous trouvons dans Paris
+        var lat = 48.863811;
+        var lng = 2.345753;
+
+        $scope.$apply(function(){
+        // Récuperation des données pour le marqueur de position
+        $scope.mylat = lat;
+        $scope.mylng = lng;
+        // Test périmetre
+        if ( (lat > 48.81229 ) && (lat < 48.905351) && (lng < 2.423) && (lng > 2.243443) )
+            {
+                //Si nous sommes dans le perimetre : on centre sur notre position
+                $scope.lat = lat;
+                $scope.lng = lng;
+                $scope.zoom = 15;
+            }
+            else
+            {
+                //Si nous sommes hors du perimetre : on centre sur Paris
+                $scope.lat =48.856614;
+                $scope.lng = 2.3522219000000177;
+                $scope.zoom = 12;
+            }
+        });
+
+        var mapOptions = {
+            zoom: $scope.zoom,
+            center: new google.maps.LatLng($scope.lat,$scope.lng),
+            mapTypeId: google.maps.MapTypeId.TERRAIN
         }
-        $scope.movies = data.data;
-    })
 
-    var mapOptions = {
-        zoom: 13,
-        center: new google.maps.LatLng(48.8566140,2.3522219),
-        mapTypeId: google.maps.MapTypeId.TERRAIN
-    }
+        $scope.map = new google.maps.Map(document.getElementById('map'), mapOptions);
 
-    $scope.map = new google.maps.Map(document.getElementById('map'), mapOptions);
+        //Affichage de notre localisation
+        var myLoc = new google.maps.Marker({
+            map: $scope.map,
+            position: {lat: $scope.mylat, lng: $scope.mylng},
+            icon: 'http://maps.google.com/mapfiles/ms/icons/green-dot.png',
+            title: "Votre position",
+        });
+
+//////////////////////////////////////MEGANE BDD///////////////////////////////////////
 
     $scope.markers = [];
 
     var infoWindow = new google.maps.InfoWindow();
 
     var createMarker = function(info){
-      var marker = new google.maps.Marker({
-          map: $scope.map,
-          position: new google.maps.LatLng(info.lat, info.lng),
-          title: info.titre
-      });
-/* TEST POUR RECUPERER DU CONTENU AVEC API */
+        var marker = new google.maps.Marker({
+            map: $scope.map,
+            position: new google.maps.LatLng(info.lat, info.lng),
+            title: info.titre
+        });
 
-       $scope.information = [];
-       $http.get('http://www.omdbapi.com/?t='+info.titre+'&y=&plot=full&r=json')
-            .success(function(data) {
-                 $scope.information = data;
-            })
-            .error(function(data,status,error,config){
-                $scope.information = [{heading:"Error",description:"Could not load json data"}];
-            });
-
-
-/*FIN */
-    marker.content = '<div class="infoWindowContent">'
-+ information.real + information.Year + information.Genre+ '</div>';
+        var data = [];
+        data = jsonSyncLoad( "http://www.omdbapi.com/?t="+info.titre+"&y=&plot=full&r=json" );
+        if(data.Director == undefined ) {
+            marker.content = '<div class="infoWindowContent"><p>'+ info.real+'</p></div>';
+        }
+        else {
+            marker.content = '<div class="infoWindowContent"><p>'+ data.Director+'</p> <p>' + data.Year +'</p><p>'+ data.Genre+ '</p><p>'+data.Actors+'</p></div>';
+        }
 
         google.maps.event.addListener(marker, 'click', function(){
             infoWindow.setContent('<h2>' + marker.title + '</h2>' + marker.content);
@@ -49,15 +82,27 @@ app.controller('MapCtrl',['$scope', 'filmsDataService', function($scope, filmsDa
         });
 
         $scope.markers.push(marker);
+    }
 
+    function jsonSyncLoad( pFile ) {
+        var mime      =    "application/json"   ;
+        var xmlhttp   =   new XMLHttpRequest()  ;
+
+        xmlhttp.open("GET",pFile,false);
+        if (xmlhttp.overrideMimeType)
+            xmlhttp.overrideMimeType( mime );
+
+        xmlhttp.send();
+        return (xmlhttp.status==200) ? JSON.parse( xmlhttp.responseText ) : null ;
     }
 
     $scope.openInfoWindow = function(e, selectedMarker){
         e.preventDefault();
         google.maps.event.trigger(selectedMarker, 'click');
     }
-
+    });
 }]);
+
 
 /*
 function calcRoute() {
